d20ca5676d0ffd54694e9cc9f83a9e7ef166f785
Md Alamin <alamin3636md@gmail.com>
2024-08-13 01:27:36 +0600

    first practice done

---
+++ Project-1/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import HeroSection from './component/Hero';
+import Nevigation from './component/navigation'
+const App = () =>{
+    return(
+    <div>
+       <Nevigation />
+       <HeroSection />
+    </div>
+    )
+};
+export default App;
