cd798f985462cbbf148e938327cd53afa25a9dd6
Md Alamin <alamin3636md@gmail.com>
2024-08-20 01:18:10 +0600

    finally done this project

---
+++ Project-1/src/components/Herosection.jsx
@@ -0,0 +1,25 @@
+const Herosection = () =>{
+    return (
+        <div className="main">
+            <div className="Herocontent">
+                <div className="Herocontent-h1">YOUR FEET DESERVE THE BEST</div>
+                <div className="Herocontent-p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae ea, repellendus alias animi tenetur harum suscipit eius explicabo iste atque?</div>
+                <div className="hero-btn">
+                    <button className="shop">shop Now</button>
+                    <button className="catagory">Catagory</button>
+                </div>
+                <div className="small-content">
+                    Lorem ipsum dolor sit amet.
+                </div>
+                <div className="pic">
+                    <img src="images/brand_logo.png" alt="" />
+                    <img src="images/flipkart.png" alt="" />
+                </div>
+            </div>
+            <div className="rightpart">
+                <img src="/images/hero-image.png" alt="" />
+            </div>
+        </div>
+    );
+};
+export default Herosection;
